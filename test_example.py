f13fc9e0bc9d31a190abb8ecdadbf76429c3e2bf
ZeamSTV <jeamsont2017@gmail.com>
2024-11-29 09:54:38 +0700

    upload test fail at example2

--- test_example.py
+++ test_example.py
@@ -1,3 +1,6 @@
 from example import hello
+from example2 import helloTeacher
 def test_hello():
-    assert hello() == 'Hello, My Team'
+    assert hello() == 'Hello, My Team!'
+def helloTeacher():
+    assert helloTeacher == 'Hello, Teacher'
